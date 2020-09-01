ff0ae48bcfd01dd0cd21732cadbe82c563d8b196
Otto Salminen <otto.salminen@G0684.local>
2020-09-01 11:04:41 +0300

    Github link added to css source header

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ else
     # Compress gauge.css
     cleancss -o gauge.min.css gauge.css
     # Prepend header to gauge.min.css
-    printf '%s\n%s\n%s\n%s\n%s\n%s\n' "/**" " * Version: ${1}" " * Author: Otto Salminen - 2020" " * License: MIT" " */" "$(cat gauge.min.css)" >gauge.min.css
+    printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n' "/**" " * Version: ${1}" " * Author: Otto Salminen - 2020"  " * Github: https://github.com/rotvalli/css-gauge" " * License: MIT" " */" "$(cat gauge.min.css)" >gauge.min.css
 
     # Compress gauge-glossy.css
     cleancss -o gauge-glossy.min.css gauge-glossy.css
